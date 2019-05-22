f65ecf0145a72ae528c51f6232990e595bcfc630
Fred Moyer <fred@redhotpenguin.com>
2019-05-21 21:34:58 -0700

    Add java, add invocations

---
+++ helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+/*
+ * prints hello world
+ * run this with 'java HelloWorld.java'
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
