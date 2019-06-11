774352e456a2055ed6abe3e0024042e3da852106
Fred Moyer <fred@redhotpenguin.com>
2019-06-10 22:10:50 -0700

    comment assembly example

--- helloworld/hello_world_osx_64.asm
+++ helloworld/hello_world_osx_64.asm
@@ -1,4 +1,5 @@
-; referenced from https://gist.github.com/FiloSottile/7125822
+; referenced from https://filippo.io/making-system-calls-from-assembly-in-mac-os-x/
+; os x assembly conventions read https://www.raywenderlich.com/615-assembly-register-calling-convention-tutorial
 ; /usr/local/bin/nasm -f macho64 hello_world_osx_64.asm && ld -macosx_version_min 10.7.0 -lSystem -o 64 hello_world_osx_64.o && ./64
 global start
 
@@ -6,18 +7,18 @@ global start
 section .text
 
 start:
-    mov     rax, 0x2000004 ; write
-    mov     rdi, 1 ; stdout
-    mov     rsi, msg
-    mov     rdx, msg.len
-    syscall
+    mov     rax, 0x2000004 ; set register rax to the write syscall number
+    mov     rdi, 1 ; push the stdout file descriptor onto the stack
+    mov     rsi, msg ; put the hello world message into the rsi register
+    mov     rdx, msg.len ; hello world message length in rdx register
+    syscall ; execute execute execute
 
-    mov     rax, 0x2000001 ; exit
-    mov     rdi, 0
-    syscall
+    mov     rax, 0x2000001 ; set register rax to the exit syscall number
+    mov     rdi, 0 ; push the return code onto the stack
+    syscall ; execute execute execute
 
 
 section .data
 
-msg:    db      "Hello World Assembly!", 10
-.len:   equ     $ - msg
+msg:    db      "Hello World Assembly!", 0xa ; 0xa is newline
+.len:   equ     $ - msg ; set the length of the string
